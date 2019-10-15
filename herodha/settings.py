03ec117b349bfb1cdb0bc909e09a2c5571fce6c2
lokesh <lksh2812@gmail.com>
2019-10-15 17:35:59 +0530

    configured templates directory

--- herodha/settings.py
+++ herodha/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'stock_trading.apps.StockTradingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'herodha.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ stock_trading/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StockTradingConfig(AppConfig):
+    name = 'stock_trading'
