091cb08a3b074c15147654ef3f84a2d1f7c0adc2
lokesh <lksh2812@gmail.com>
2019-10-16 10:14:36 +0530

    setup database and made migrations

--- herodha/settings.py
+++ herodha/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'herodha.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv("DB_ENGINE"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
