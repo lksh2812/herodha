26689476d4c774175a797f99b50ab833ec018795
panjasaurabh@gmail.com <panjasaurabh@gmail.com>
2019-10-16 10:43:21 +0530

    added get_quote.html and style.css

--- stock_trading/views.py
+++ stock_trading/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def get_quote(request):
+    return render(request, 'get_quote.html', {'data' : 'Saurabh Panja'})
