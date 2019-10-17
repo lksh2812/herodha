6c1968a1e31abf3399e64de983b8c61e52942ead
Sandeep Jangir <sandeep.jangir@mountblue.io>
2019-10-17 17:43:07 +0530

    added BuyTransaction model

--- stock_trading/models.py
+++ stock_trading/models.py
@@ -7,3 +7,16 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class BuyTransaction(models.Model):
+    
+    company_name = models.CharField(max_length=100)
+    company_code = models.CharField(max_length=50)
+    qty = models.IntegerField()
+    last_price = models.FloatField()
+    Total = models.FloatField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '<User:{} Transactions {}>'.format(self.user_id__username, self.company_name)
