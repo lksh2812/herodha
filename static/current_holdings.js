1f1454cc5d850aeb0ad1f5945817a9af909cee30
panjasaurabh@gmail.com <panjasaurabh@gmail.com>
2019-10-19 16:36:36 +0530

    added fetch to update profit and current price on realtime

--- static/current_holdings.js
+++ static/current_holdings.js
@@ -1,5 +1,31 @@
 window.onload = () =>{
-    console.log("I am working")
-    let profile = document.querySelector("#profile");
-    console.log(profile)
+    setInterval(getAllCurrentPriceOnPage, 3000)
 }
+
+const getAllCurrentPriceOnPage = () =>{
+    console.log("gettting Current price")
+    let companySymbols = document.querySelectorAll('#company-code'),
+        currentPrices = document.querySelectorAll('#current-price'),
+        profit = document.querySelectorAll('#profit'),
+        qty = document.querySelectorAll('#qty'),
+        total = document.querySelectorAll('#total');
+    for(let i = 0; i < companySymbols.length; i++){
+        companySymbol = companySymbols[i].innerText;
+        getCurrentPriceAndProfit(companySymbol,currentPrices[i],profit[i],qty[i],total[i])
+    }
+}
+
+async function getCurrentPriceAndProfit (companyCode, currentPriceDom, profitDom, qtyDom, totalDom){
+    let response = await fetch(`/get_current_price/${companyCode}`);
+    let priceJson = await response.json();
+    let price = await priceJson['stock_data']['lastPrice']
+    // console.log(price['lastPrice'])
+    currentPriceDom.innerText = price
+    qty = Number(qtyDom.innerText)
+    currentPrice = Number(currentPriceDom.innerText)
+    sellingPrice = Number(totalDom.innerText)
+    buyingPrice = qty * currentPrice
+    profit = sellingPrice - buyingPrice
+    profitDom.innerText = Math.round(profit * 100) / 100;
+}
+
