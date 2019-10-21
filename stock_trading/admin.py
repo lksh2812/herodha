3ea0abee01e8dabd343c4e45ef4fa0f0add99126
lokesh <lksh2812@gmail.com>
2019-10-21 14:59:23 +0530

    Registered all models in admin.py

--- stock_trading/admin.py
+++ stock_trading/admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm
-from .models import User
+from .models import User, SellTransaction, BuyTransaction, Bookmark
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
@@ -14,3 +14,6 @@ class CustomUserAdmin(UserAdmin):
     list_display = ['email', 'username',]
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(SellTransaction)
+admin.site.register(BuyTransaction)
+admin.site.register(Bookmark)
