484480d417acb21641332856fb335d3ba84d9ce0
lokesh <lksh2812@gmail.com>
2019-10-22 11:05:19 +0530

    created module to convert nse stocks to csv file

---
+++ generate_nse_stocks.py
@@ -0,0 +1,24 @@
+from nsetools import Nse
+nse = Nse()
+import os
+import csv
+
+# update elasticsearch database with all currently present stocks in NSE
+def update_stocks():
+    file = os.getcwd()
+    all_stock_codes = nse.get_stock_codes()
+    
+    csv_data = []
+    for code in all_stock_codes:
+        csv_data.append([code, all_stock_codes[code]])
+    
+    with open(os.path.join(file,'all_stocks_code.csv'), 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(csv_data)
+    f.close()
+    return True
+
+
+if __name__ == "__main__":
+   status = update_stocks()
+
